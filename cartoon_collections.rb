53c330d777212a6a442e0488a9e6d4ada22aec54
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 01:12:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   result = false
-  result = array.all? do |word| word.length>=4 end
+  result = array.all? do |word| word.length>4 end
   result
 end
 
