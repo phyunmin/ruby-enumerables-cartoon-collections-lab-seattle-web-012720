c03df52bc477ee5a500983f5699e46ac0cee8e63
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 01:20:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  str = ""
+  array.each_with_index do |item, index| str = puts "/#{index+1}.*#{item}/" end
+  str
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  test = []
+  array.map! do |data| data.capitalize end
+  array.each do |data| test.push("#{data}!") end
+  test
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? do |word| word.length>4 end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i=0
+  result = nil
+  while i<cheese_types.length do
+    if(ingredients.include?(cheese_types[i]))
+      result = cheese_types[i]
+      break
+    end
+    i+=1
+  end
+  return result
 end
