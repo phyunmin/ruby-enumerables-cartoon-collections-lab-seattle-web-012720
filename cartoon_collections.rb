27e0c83e78d2e57e0c9ef93ed263efcf20c9184c
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 00:32:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   new = []
   new = array.each_with_index do |data, index| new.push("#{index+1}.#{data}") 
-  array.map do |data| puts "#{data}" end
+  new.map do |data| puts "#{data}" end
 end
 
 def summon_captain_planet# code an argument here
