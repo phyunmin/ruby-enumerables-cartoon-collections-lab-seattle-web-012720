e086b1ca8f774d881c9896040ce7964517de054e
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 01:09:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(array)# code an argument here
   test
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.all? do |word| word.length>4 end
 end
 
 def find_the_cheese# code an argument here
