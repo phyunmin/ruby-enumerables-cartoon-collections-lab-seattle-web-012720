ed07dbdc8a89b72244afb8124cb4c54ffe4846fc
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 01:17:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.include?
+  if(ingredients.include?()
 end
